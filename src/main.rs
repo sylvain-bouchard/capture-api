5f10a562259a88a6067941d86a7cc9e0d969c790
Sylvain Bouchard <sylvain.bouchard@live.ca>
2024-06-29 15:06:40 -0400

    feat: record MP4 video for 10 s

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use axum::{
 use gstreamer as gst;
 use gstreamer::prelude::Cast;
 use gstreamer::prelude::ElementExt;
+use gstreamer::prelude::ElementExtManual;
 use gstreamer::prelude::GstObjectExt;
 
 use serde::{Deserialize, Serialize};
@@ -38,7 +39,7 @@ fn create_stream_pipeline() -> Result<(), gst::glib::error::Error> {
     gst::init().unwrap();
 
     let pipeline = gst::parse::launch(&format!(
-        "avfvideosrc ! videoconvert ! queue ! x264enc ! mp4mux ! filesink location=recording.mp4"
+        "avfvideosrc ! videoconvert ! queue ! x264enc ! mp4mux ! filesink location=output/recording.mp4"
     ))?
     .downcast::<gst::Pipeline>()
     .expect("type error");
@@ -46,6 +47,11 @@ fn create_stream_pipeline() -> Result<(), gst::glib::error::Error> {
     pipeline
         .set_state(gst::State::Playing)
         .expect("Unable to set the pipeline to the `Playing` state");
+
+    // Wait for a short while to simulate some processing (e.g., 10 seconds)
+    std::thread::sleep(std::time::Duration::from_secs(10));
+
+    pipeline.send_event(gst::event::Eos::new());
 
     for msg in pipeline.bus().unwrap().iter_timed(gst::ClockTime::NONE) {
         use gst::MessageView;
