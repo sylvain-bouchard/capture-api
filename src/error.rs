b802fc520f19e402231633266c250ec7067a7d5a
Sylvain Bouchard <sylvain.bouchard@ville.montreal.qc.ca>
2024-07-02 17:55:33 -0400

    feat: introduce error enum

---
+++ src/error.rs
@@ -0,0 +1,14 @@
+use axum::{http::StatusCode, response::{ IntoResponse, Response }};
+
+#[derive(Debug)]
+pub enum Error {
+  LoginFail,
+}
+
+impl IntoResponse for Error {
+  fn into_response(self) -> Response {
+      println!("--> {:12} - {self:?}", "INTO_RES");
+
+      (StatusCode::INTERNAL_SERVER_ERROR, "Unhandled client").into_response()
+  }
+}
