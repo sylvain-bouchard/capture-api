0d7ff62c0a33b63a65ddd521a164e0debadfdbeb
Sylvain Bouchard <sylvain.bouchard@ville.montreal.qc.ca>
2024-07-02 17:56:35 -0400

    refactor: rename unit test function -> test_users_endpoint

--- tests/api.rs
+++ tests/api.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 
 #[tokio::test]
-async fn test_endpoint() -> Result<()> {
+async fn test_users_endpoint() -> Result<()> {
     let client = httpc_test::new_client("http://localhost:3000")?;
 
     client.do_get("/users").await?;
