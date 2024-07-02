f110d8041d85eec91c3435b95611106a88f43e82
Sylvain Bouchard <sylvain.bouchard@live.ca>
2024-07-02 06:43:29 -0400

    style: pretty print

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use axum::{
-    extract::Path, http::StatusCode, response::{Html, IntoResponse}, routing::{get_service, post}, Json, Router
+    extract::Path,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::{get_service, post},
+    Json, Router,
 };
 use configuration::load_config;
 use gstreamer as gst;
@@ -7,7 +11,6 @@ use gstreamer::prelude::Cast;
 use gstreamer::prelude::ElementExt;
 use gstreamer::prelude::ElementExtManual;
 use gstreamer::prelude::GstObjectExt;
-
 use serde::{Deserialize, Serialize};
 use tower_http::services::ServeDir;
 
@@ -29,15 +32,18 @@ async fn main() {
         .merge(routes_users())
         .fallback_service(routes_static());
 
-    let address = format!("{}:{}", app_configuration.api.local_ip, app_configuration.api.port);
+    let address = format!(
+        "{}:{}",
+        app_configuration.api.local_ip, app_configuration.api.port
+    );
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
 fn routes_users() -> Router {
-	Router::new()
-		.route("/users/:name", post(handle_get_user))
+    Router::new()
+        .route("/users/:name", post(handle_get_user))
         .route("/users", post(handle_create_user))
 }
 
@@ -88,14 +94,12 @@ fn create_stream_pipeline() -> Result<(), gst::glib::error::Error> {
 }
 
 async fn handle_get_user(Path(name): Path<String>) -> impl IntoResponse {
-	println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
 
-	Html(format!("Hello2 <strong>{name}</strong>"))
+    Html(format!("Hello2 <strong>{name}</strong>"))
 }
 
-async fn handle_create_user(
-    Json(payload): Json<CreateUser>,
-) -> impl IntoResponse {
+async fn handle_create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
     let user = User {
         id: 1337,
         username: payload.username,
