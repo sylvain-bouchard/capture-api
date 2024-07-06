0d8c149a3f823f3f0ed4e6f21000ebe3899f1b90
Sylvain Bouchard <sylvain.bouchard@ville.montreal.qc.ca>
2024-07-06 11:13:09 -0400

    feat: add error variant

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,18 @@
-use axum::{http::StatusCode, response::{ IntoResponse, Response }};
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 
 #[derive(Debug)]
 pub enum Error {
-  LoginFail,
+    LoginFail,
+    UserNotFound { username: String },
 }
 
 impl IntoResponse for Error {
-  fn into_response(self) -> Response {
-      println!("--> {:12} - {self:?}", "INTO_RES");
+    fn into_response(self) -> Response {
+        println!("--> {:12} - {self:?}", "INTO_RES");
 
-      (StatusCode::INTERNAL_SERVER_ERROR, "Unhandled client").into_response()
-  }
+        (StatusCode::INTERNAL_SERVER_ERROR, "Unhandled client").into_response()
+    }
 }
