0144c56926fa3806822310b7e23467669dd805e7
Sylvain Bouchard <sylvain.bouchard@ville.montreal.qc.ca>
2024-07-06 11:13:49 -0400

    refactor: extract user model into the models crate

---
+++ src/models/user.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug)]
+pub struct UserForCreate {
+    pub username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize, Debug)]
+pub struct User {
+    pub id: u64,
+    pub username: String,
+}
