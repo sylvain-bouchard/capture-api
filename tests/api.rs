2cfddfcb6f9e275a1f92f15cd3ecccd520167fcd
Sylvain Bouchard <sylvain.bouchard@live.ca>
2024-07-09 18:21:18 -0400

    fix: user controller endpoint unit tests

--- tests/api.rs
+++ tests/api.rs
@@ -6,7 +6,7 @@ async fn test_create_user() -> Result<()> {
     let client = httpc_test::new_client("http://localhost:3000")?;
 
     let create_user_request = client.do_post(
-        "/users",
+        "/api/users",
         json!({
             "username": "sylvainb"
         }),
@@ -20,7 +20,7 @@ async fn test_create_user() -> Result<()> {
 async fn test_read_user() -> Result<()> {
     let client = httpc_test::new_client("http://localhost:3000")?;
 
-    let read_user_request = client.do_get("sylvainb");
+    let read_user_request = client.do_get("/api/users");
     read_user_request.await?.print().await?;
 
     Ok(())
