84a4d6119d6ded0290e91656be7d04c67efde0fb
Sylvain Bouchard <sylvain.bouchard@live.ca>
2024-12-14 17:56:40 +0800

    refactor: get rid of some #unwrap in user service

--- src/features/users/user_service.rs
+++ src/features/users/user_service.rs
@@ -1,7 +1,7 @@
 use std::sync::{Arc, Mutex};
 use thiserror::Error;
 
-use super::{user_dto::UserForCreate, user_entity::User};
+use super::{user_dto::UserDto, user_entity::User};
 
 #[derive(Debug, Error)]
 pub enum UserServiceError {
@@ -10,11 +10,13 @@ pub enum UserServiceError {
     #[allow(dead_code)]
     #[error("Internal server error")]
     InternalServerError,
+    #[error("Lock poisoned")]
+    LockPoisoned,
 }
 
 #[derive(Clone)]
 pub struct UserService {
-    user_store: Arc<Mutex<Vec<Option<User>>>>,
+    user_store: Arc<Mutex<Vec<User>>>,
 }
 
 impl UserService {
@@ -24,43 +26,49 @@ impl UserService {
         })
     }
 
-    pub async fn create_user(
-        &self,
-        user_for_create: UserForCreate,
-    ) -> Result<User, UserServiceError> {
+    pub async fn create_user(&self, user_dto: UserDto) -> Result<User, UserServiceError> {
         let mut store = self.user_store.lock().unwrap();
 
         let id = store.len() as u64;
         let user = User {
             id,
-            username: user_for_create.username,
+            username: user_dto.username,
         };
-        store.push(Some(user.clone()));
+        store.push(user.clone());
 
         Ok(user)
     }
 
     pub async fn read_user(&self, id: u64) -> Result<User, UserServiceError> {
-        let store = self.user_store.lock().unwrap();
+        let store = self
+            .user_store
+            .lock()
+            .map_err(|_| UserServiceError::LockPoisoned)?;
 
-        let user = store.get(id as usize).unwrap().clone();
-
-        user.ok_or(UserServiceError::UserNotFound(id))
+        match store.get(id as usize) {
+            Some(user) => Ok(user.clone()),
+            None => Err(UserServiceError::UserNotFound(id)),
+        }
     }
 
     pub async fn list_users(&self) -> Result<Vec<User>, UserServiceError> {
         let store = self.user_store.lock().unwrap();
 
-        let users = store.iter().filter_map(|user| user.clone()).collect();
+        let users = store.iter().cloned().collect();
 
         Ok(users)
     }
 
     pub async fn delete_user(&self, id: u64) -> Result<User, UserServiceError> {
-        let mut store = self.user_store.lock().unwrap();
+        let mut store = self
+            .user_store
+            .lock()
+            .map_err(|_| UserServiceError::LockPoisoned)?;
 
-        let user = store.get_mut(id as usize).and_then(|user| user.take());
-
-        user.ok_or(UserServiceError::UserNotFound(id))
+        if (id as usize) < store.len() {
+            Ok(store.remove(id as usize)) // Remove the user and return it
+        } else {
+            Err(UserServiceError::UserNotFound(id))
+        }
     }
 }
