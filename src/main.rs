c3ed1c850e67ac51362ed5d582515e616bcb4b52
Sylvain Bouchard <sylvain.bouchard@live.ca>
2024-12-16 16:53:44 +0800

    feat: add first main unit test

--- src/main.rs
+++ src/main.rs
@@ -39,3 +39,51 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt;
+    use tower::ServiceExt;
+
+    use crate::{application::Application, configuration::load_config};
+
+    #[tokio::test]
+    async fn it_should_create_user() -> Result<(), Box<dyn std::error::Error>> {
+        let configuration = load_config()?;
+
+        let router = Application::new()
+            .with_configuration(&configuration)
+            .build_router();
+
+        // GIVEN
+        let new_user_json = r#"{
+            "id": "123",
+            "username": "jane"
+        }"#;
+
+        // WHEN
+        let response = router
+            .oneshot(
+                Request::builder()
+                    .uri("/api/users")
+                    .method("POST")
+                    .header("Content-Type", "application/json")
+                    .body(Body::from(new_user_json))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+
+        // THEN
+        assert_eq!(response.status(), StatusCode::CREATED);
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(body.as_ref(), b"jane");
+
+        Ok(())
+    }
+}
