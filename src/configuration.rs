ea168acbf38f65539edb2d7f887837405d8843fb
Sylvain Bouchard <sylvain.bouchard@live.ca>
2024-12-18 15:36:17 +0800

    feat: add datasource configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,6 @@
 use config::{Config, ConfigError, Environment, File};
 use dotenv::dotenv;
+use sea_orm::ConnectOptions;
 use serde::Deserialize;
 
 #[derive(Clone, Debug, Deserialize)]
@@ -8,6 +9,7 @@ pub struct AppConfiguration {
     pub debug: Option<bool>,
     pub api: ApiConfiguration,
     pub media: MediaConfiguration,
+    pub data_source: DataSourceConfiguration,
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -23,6 +25,38 @@ pub struct ApiConfiguration {
 pub struct MediaConfiguration {
     pub enabled: bool,
     pub recording_duration: u16,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct DataSourceConfiguration {
+    pub host: String,
+    pub port: u16,
+    pub user: String,
+    pub password: String,
+    pub database: String,
+}
+
+impl DataSourceConfiguration {
+    pub fn get_connection_options(uri: &str) -> ConnectOptions {
+        let mut options = ConnectOptions::new(uri.to_owned());
+        options.max_connections(100).sqlx_logging(true);
+
+        options
+    }
+
+    pub fn get_connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            &self.user, &self.password, &self.host, self.port, &self.database
+        )
+    }
+
+    pub fn get_connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            &self.user, &self.password, &self.host, self.port
+        )
+    }
 }
 
 pub fn load_config() -> Result<AppConfiguration, ConfigError> {
