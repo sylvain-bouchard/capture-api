118cd5219bb87072b492e546d9d986d1f44200b3
Sylvain Bouchard <sylvain.bouchard@live.ca>
2024-12-19 13:38:02 +0800

    feat(orm): add user record

--- src/features/users/mod.rs
+++ src/features/users/mod.rs
@@ -1,4 +1,5 @@
 pub mod user_dto;
 pub mod user_entity;
+pub mod user_record;
 pub mod user_routes;
 pub mod user_service;
---
+++ src/features/users/user_record.rs
@@ -0,0 +1,21 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "user")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    #[serde(skip_deserializing)]
+    pub id: u64,
+    pub username: String,
+    pub password_hash: String,
+    #[sea_orm(column_type = "Text")]
+    pub text: String,
+    pub created_at: DateTimeUtc,
+    pub updated_at: DateTimeUtc,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
